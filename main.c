c14b7ae30fbd8049ecffbcdebc97ccc7afc17e6f
jannelouisea <janne.louise.a@gmail.com>
2017-09-25 22:04:21 -0400

    Added main comments

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
 
+char ** parseLine(char * line) {
+    // use example from internet but without the extra stuff
+    // make sure to check for space skips
+}
+
 int main(int argc, char * argv[]) {
-    return 0;
+    // Check number of arguments
+    if (argc != 4) {
+        printf("ERROR: Incorrect usage.\nUsage: ./p4 <filepath> <pdbr> <virtual adress>");
+    }
+
+    // open file
+
+    // char ** paTable;
+    // paTable = malloc((string of char *) * numOfPages * numOfEntries)
+
+    // while getc to get file in each line
+    // get an array of tokens...
+    // skip the next two tokens
+
+    // strdup to paTable
+
+    // assert that the number of index equals
+
+    // get the base register
+
+    // translate the virtual address to components
+    // turn virtual address into an integer
+
+    // use masks and right shift
+
+    // validBit mask 128
+    // pfn mask 127
+
 }
