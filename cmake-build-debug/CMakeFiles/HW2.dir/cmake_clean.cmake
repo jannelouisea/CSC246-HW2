e46ba8c0ffa6a973b7d13d655f3f81f25df5b2a5
jannelouisea <janne.louise.a@gmail.com>
2017-09-25 21:47:24 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/HW2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW2.dir/main.c.o"
+  "HW2.pdb"
+  "HW2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
