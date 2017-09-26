7564859d93e39797ccedcb0be64184eb6a7dc3cf
jannelouisea <janne.louise.a@gmail.com>
2017-09-25 22:53:58 -0400

    Able to read file and store tokens

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 /* Error status */
 #define INCORRECT_USAGE -1
@@ -17,6 +19,39 @@
 char ** parseLine(char * line) {
     // use example from internet but without the extra stuff
     // make sure to check for space skips
+
+    char * delim = " \n";
+    size_t count = 0;
+    char * tmp = line;
+    char ** result = 0;
+
+    while (*tmp) {
+        if (*tmp == *delim && *(tmp + 1) != ' ') {
+            count++;
+        }
+        tmp++;
+    }
+    // printf("Count: %d\n", count);
+
+    result = malloc(sizeof(char *) * count);
+
+    if (result) {
+        size_t idx  = 0;
+        char * token = strtok(line, delim);
+
+        while (token) {
+            assert(idx < count);
+            *(result + idx) = strdup(token);
+            *(result + idx) = strdup(token);
+            *(result + idx) = strdup(token);
+            idx++;
+            token = strtok(0, delim);
+        }
+        assert(idx == count);
+        *(result + idx) = 0;
+    }
+
+    return result;
 }
 
 int main(int argc, char * argv[]) {
@@ -34,9 +69,27 @@ int main(int argc, char * argv[]) {
         return UNABLE_TO_OPEN_FILE;
     }
 
-    char ** paTable;
-    paTable = malloc(sizeof(char *) * NUM_OF_PAGES * NUM_OF_ENTRIES);
+    char ** paTable = NULL;
+    paTable         = malloc(sizeof(char *) * NUM_OF_PAGES * NUM_OF_ENTRIES);
+    // check if paTable is null..?
+    int paTableIdx  = 0;
 
+    char * line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, fp) != -1) {
+        printf("%s", line);
+        char ** tokens = parseLine(line);
+        tokens += 2;
+        while (*tokens) {
+            // strdup token to paTable
+            *(paTable + paTableIdx++) = strdup(*tokens);
+            // paTableIdx++;
+            tokens++;
+        }
+    }
+    assert(paTableIdx == NUM_OF_ENTRIES * NUM_OF_PAGES);
+
+    fclose(fp);
     // while getc to get file in each line
     // get an array of tokens...
     // skip the next two tokens
